5d436f4443f98b26c5af244e75fd312f21cac233
Kazuki Tsunemi <ksilverwall@gmail.com>
2015-05-16 18:44:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ksilver/gles20test/MainView.java
@@ -0,0 +1,31 @@
+package com.example.ksilver.gles20test;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainView extends GLSurfaceView {
+
+    public MainView(Context context) {
+        super(context);
+        setEGLContextClientVersion(2);
+        setRenderer(new Renderer());
+    }
+    private class Renderer implements GLSurfaceView.Renderer {
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            GLES20.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        }
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            GLES20.glViewport(0,0,width,height);
+        }
+        @Override
+        public void onDrawFrame(GL10 gl) {
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        }
+    }
+}
